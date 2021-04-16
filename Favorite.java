b63c5e71b2dfd6cc2df13099b5a7ba0bacbe6149
sakasaka0722 <yukisakai0722@gmail.com>
2021-04-17 00:06:17 +0900

    課題1-2

---
+++ Favorite.java
@@ -0,0 +1,8 @@
+public class Favorite{
+  public static void main(String[] args) {
+    String favorite = "巨峰";
+    String reason = "甘くて果汁が口の中に広がる感じが好きだから。秋を感じるから。";
+    System.out.println("好きな食べ物 : " + favorite);
+    System.out.println("理由 : " + reason);
+  }
+}
